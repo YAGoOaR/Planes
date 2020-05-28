5e5fba7839e60366879808e3632da03fb9b6225f
YAGoOaR <yegorgribenko68@gmail.com>
2020-05-28 15:30:15 +0300

    added in-game controls information

--- Assets/Scripts/Menu/Button1.cs
+++ Assets/Scripts/Menu/Button1.cs
@@ -3,9 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class Button1 : MonoBehaviour
 {
+    public GameObject controlsPrefab;
 
     public void StartGame() {
         SceneManager.LoadScene(1);
+    }
+
+    public void ShowControls()
+    {
+        GameObject.Instantiate(controlsPrefab, transform.parent);
     }
 
     public void ExitGame()
---
+++ Assets/Scripts/Menu/CloseButton.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class CloseButton : MonoBehaviour
+{
+    public void close() {
+        Object.Destroy(transform.parent.gameObject);
+    }
+}
